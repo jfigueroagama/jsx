749d20a0804ca3dee1f8c91b6adb4d9f6600ff33
Josué Gama <josue.figueroa@magmalabs.io>
2020-03-18 18:01:40 -0600

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// We can use a function that will be called in our App
+//function getButtonText() {
+////return "Click on me!";
+//}
+
 // Create a React component
 const App = function() {
-  return <div>Hi there!</div>;
+  const buttonText = { text: "Click me!" };
+  const labelText = "Enter name:";
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
+
+// Variables can be referenced between {}
 
 // Take the React component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
